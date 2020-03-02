d4fdad615b28a0cefc32b6a7a24e1d09660a6d02
zhoujiafa <41611932+zhoujiafa@users.noreply.github.com>
2020-03-02 09:18:55 +0800

    最新更新信息

---
+++ src/main/java/com/springcloud/bean/dos/DeliverGoods.java
@@ -0,0 +1,10 @@
+package com.springcloud.bean.dos;
+
+/**
+ * @Author darren.zhou
+ * @Description:
+ * @ClassName: $ClassName$
+ * @Create: $Date$ $Time$
+ */
+public class DeliverGoods {
+}
---
+++ src/main/java/com/springcloud/service/DeliverGoodsService.java
@@ -0,0 +1,10 @@
+package com.springcloud.service;
+
+/**
+ * @Author darren.zhou
+ * @Description:
+ * @ClassName: $ClassName$
+ * @Create: $Date$ $Time$
+ */
+public interface DeliverGoodsService {
+}
